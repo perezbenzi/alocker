b4fa29a40166d522859f4c777ec5a0d44ae85e82
perezbenzif <perezbenzif@gmail.com>
2023-07-10 23:39:05 -0300

    create audioUploader component

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqna6sjqdHq3c3QM3xWbVB7IIzvJHyIXs",
-  authDomain: "alocker-bd7b8.firebaseapp.com",
-  projectId: "alocker-bd7b8",
-  storageBucket: "alocker-bd7b8.appspot.com",
-  messagingSenderId: "156208158049",
-  appId: "1:156208158049:web:4663075964f1379e73a120",
-  measurementId: "G-BHQ9B5QDCL"
+  apiKey: 'AIzaSyDqna6sjqdHq3c3QM3xWbVB7IIzvJHyIXs',
+  authDomain: 'alocker-bd7b8.firebaseapp.com',
+  projectId: 'alocker-bd7b8',
+  storageBucket: 'alocker-bd7b8.appspot.com',
+  messagingSenderId: '156208158049',
+  appId: '1:156208158049:web:4663075964f1379e73a120',
+  measurementId: 'G-BHQ9B5QDCL'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+
+export { storage, firebase as default };
---
+++ src/audioUploader.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { storage } from './Firebase';
+
+const AudioUploader = () => {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleUpload = () => {
+    if (file) {
+      const storageRef = storage.ref();
+      const fileRef = storageRef.child(file.name);
+      fileRef.put(file)
+        .then(() => {
+          console.log('Archivo cargado exitosamente');
+        })
+        .catch((error) => {
+          console.error('Error al cargar el archivo:', error);
+        });
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Subir archivo</button>
+    </div>
+  );
+};
+
+export default AudioUploader;
+
